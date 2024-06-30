c6533f0047ab0f574ae59670acd9fb4b4576824b
FirasDev <firasbenmbarek@outlook.com>
2024-06-30 14:49:12 +0200

    added asset generation and dynamic weather icon

--- lib/features/weather/application/presentation/widgets/Weather_widget.dart
+++ lib/features/weather/application/presentation/widgets/Weather_widget.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flaconi_weather_report/gen/assets.gen.dart';
 import 'package:flaconi_weather_report/app/utils/extension/context_extension.dart';
 import 'package:flaconi_weather_report/app/utils/extension/datetime_extension.dart';
 import 'package:flaconi_weather_report/app/utils/extension/int_extension.dart';
 import 'package:flaconi_weather_report/features/weather/domain/entity/weather.dart';
-import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class WeatherWidget extends StatelessWidget {
   final Weather weather;
@@ -21,10 +23,23 @@ class WeatherWidget extends StatelessWidget {
             Text(
               weather.dt.timestampToDateTime().dayName,
               style: context.typo.h2MediumSemiBold,
-            )
+            ),
+            Lottie.asset(
+              fetchLottieWeatherAnimation(weather.weather[0].icon),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  //Method to dynamically display images based on weather data
+  //Some lotties were re-used for day/night because of free filedownload limit
+  String fetchLottieWeatherAnimation(String icon) {
+    final lottieList = Assets.weather.lottie.values;
+    return lottieList
+        .where((string) => string.endsWith('$icon.json'))
+        .toList()
+        .first;
   }
 }
---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,105 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsWeatherGen {
+  const $AssetsWeatherGen();
+
+  /// Directory path: assets/weather/lottie
+  $AssetsWeatherLottieGen get lottie => const $AssetsWeatherLottieGen();
+}
+
+class $AssetsWeatherLottieGen {
+  const $AssetsWeatherLottieGen();
+
+  /// File path: assets/weather/lottie/01d.json
+  String get a01d => 'assets/weather/lottie/01d.json';
+
+  /// File path: assets/weather/lottie/01n.json
+  String get a01n => 'assets/weather/lottie/01n.json';
+
+  /// File path: assets/weather/lottie/02d.json
+  String get a02d => 'assets/weather/lottie/02d.json';
+
+  /// File path: assets/weather/lottie/02n.json
+  String get a02n => 'assets/weather/lottie/02n.json';
+
+  /// File path: assets/weather/lottie/03d.json
+  String get a03d => 'assets/weather/lottie/03d.json';
+
+  /// File path: assets/weather/lottie/03n.json
+  String get a03n => 'assets/weather/lottie/03n.json';
+
+  /// File path: assets/weather/lottie/04d.json
+  String get a04d => 'assets/weather/lottie/04d.json';
+
+  /// File path: assets/weather/lottie/04n.json
+  String get a04n => 'assets/weather/lottie/04n.json';
+
+  /// File path: assets/weather/lottie/09d.json
+  String get a09d => 'assets/weather/lottie/09d.json';
+
+  /// File path: assets/weather/lottie/09n.json
+  String get a09n => 'assets/weather/lottie/09n.json';
+
+  /// File path: assets/weather/lottie/10d.json
+  String get a10d => 'assets/weather/lottie/10d.json';
+
+  /// File path: assets/weather/lottie/10n.json
+  String get a10n => 'assets/weather/lottie/10n.json';
+
+  /// File path: assets/weather/lottie/11d.json
+  String get a11d => 'assets/weather/lottie/11d.json';
+
+  /// File path: assets/weather/lottie/11n.json
+  String get a11n => 'assets/weather/lottie/11n.json';
+
+  /// File path: assets/weather/lottie/12d.json
+  String get a12d => 'assets/weather/lottie/12d.json';
+
+  /// File path: assets/weather/lottie/12n.json
+  String get a12n => 'assets/weather/lottie/12n.json';
+
+  /// File path: assets/weather/lottie/13d.json
+  String get a13d => 'assets/weather/lottie/13d.json';
+
+  /// File path: assets/weather/lottie/13n.json
+  String get a13n => 'assets/weather/lottie/13n.json';
+
+  /// List of all assets
+  List<String> get values => [
+        a01d,
+        a01n,
+        a02d,
+        a02n,
+        a03d,
+        a03n,
+        a04d,
+        a04n,
+        a09d,
+        a09n,
+        a10d,
+        a10n,
+        a11d,
+        a11n,
+        a12d,
+        a12n,
+        a13d,
+        a13n
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const String aEnv = '.env';
+  static const $AssetsWeatherGen weather = $AssetsWeatherGen();
+
+  /// List of all assets
+  static List<String> get values => [aEnv];
+}
